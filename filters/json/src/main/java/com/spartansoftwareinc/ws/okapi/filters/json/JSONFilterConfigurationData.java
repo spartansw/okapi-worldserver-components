6249ee100eae626fddae3ecb3f32893fb8efbf10
itkachuk <itkachuk@bottomline.com>
2018-07-30 12:23:14 +0300

    Fixed serialization

--- filters/json/src/main/java/com/spartansoftwareinc/ws/okapi/filters/json/JSONFilterConfigurationData.java
+++ filters/json/src/main/java/com/spartansoftwareinc/ws/okapi/filters/json/JSONFilterConfigurationData.java
@@ -1,5 +1,5 @@
 package com.spartansoftwareinc.ws.okapi.filters.json;
 
 public class JSONFilterConfigurationData extends AbstractJSONFilterConfigurationData{
-
+    private static final long serialVersionUID = 1L;
 }
