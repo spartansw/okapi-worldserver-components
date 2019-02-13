0d308d7757499758ea18aa6c0fb6bade1e45d978
Kevin Lew <kevin@spartansoftwareinc.com>
2019-02-13 10:43:05 -0800

    Enable MT match score configuration in AutoML adapter

--- mt/googleautoml/src/main/java/com/spartansoftwareinc/ws/okapi/mt/googleautoml/WSGoogleAutoMLAdapterConfigurationUI.java
+++ mt/googleautoml/src/main/java/com/spartansoftwareinc/ws/okapi/mt/googleautoml/WSGoogleAutoMLAdapterConfigurationUI.java
@@ -1,5 +1,7 @@
 package com.spartansoftwareinc.ws.okapi.mt.googleautoml;
 
+import static com.spartansoftwareinc.ws.okapi.mt.base.WSBaseMTAdapterConfigurationData.MATCH_SCORE;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +15,7 @@ import com.idiominc.wssdk.ais.WSAisException;
 import com.idiominc.wssdk.ais.WSNode;
 import com.idiominc.wssdk.component.WSComponentConfigurationData;
 import com.spartansoftwareinc.ws.okapi.base.ui.UIElement;
+import com.spartansoftwareinc.ws.okapi.base.ui.UIRadioButton;
 import com.spartansoftwareinc.ws.okapi.base.ui.UITextField;
 import com.spartansoftwareinc.ws.okapi.mt.base.WSBaseMTAdapterConfigurationData;
 import com.spartansoftwareinc.ws.okapi.mt.base.WSBaseMTAdapterConfigurationUI;
@@ -39,9 +42,17 @@ public class WSGoogleAutoMLAdapterConfigurationUI extends WSBaseMTAdapterConfigu
         List<UIElement> elements = new ArrayList<>();
         WSGoogleAutoMLAdapterConfigurationData googleConfigData =
                 (WSGoogleAutoMLAdapterConfigurationData) configData;
+        int matchScore = googleConfigData.getMatchScore();
 
         elements.add(new UITextField(LABEL_CREDENTIAL_AIS_PATH, CREDENTIAL_AIS_PATH,
                 googleConfigData.getCredentialAisPath()).setSize(TEXT_FIELD_SIZE));
+
+        UIRadioButton.Option defaultOption = new UIRadioButton.Option("Use Translation Service Value", MATCH_SCORE_TRANSLATION_SERVICE,
+                !googleConfigData.useCustomScoring());
+        UIRadioButton.Option customOption = new UIRadioButton.Option("Use Custom Value", MATCH_SCORE_CUSTOM,
+                googleConfigData.useCustomScoring(), getCustomValueHtml(MATCH_SCORE_CUSTOM_VALUE, 0, 100, matchScore));
+        elements.add(new UIRadioButton(LABEL_MATCH_SCORE, MATCH_SCORE, defaultOption, customOption));
+
         elements.add(new UITextField(LABEL_MODEL_MAP, MODEL_MAP, googleConfigData.getModelMap())
                 .setSize(TEXT_FIELD_SIZE));
 
@@ -72,6 +83,8 @@ public class WSGoogleAutoMLAdapterConfigurationUI extends WSBaseMTAdapterConfigu
             errors = addError(LABEL_MODEL_MAP, errors);
         }
 
+        errors = parentValidateAndSave(request, configData, errors);
+
         if (errors == null) {
             WSGoogleAutoMLAdapterConfigurationData googleConfigData =
                     (WSGoogleAutoMLAdapterConfigurationData) configData;
@@ -80,5 +93,40 @@ public class WSGoogleAutoMLAdapterConfigurationUI extends WSBaseMTAdapterConfigu
             googleConfigData.setModelMap(modelMap);
         }
         return errors;
+    }
+
+    private String parentValidateAndSave(HttpServletRequest request, WSBaseMTAdapterConfigurationData configData, String errors) {
+        boolean useCustomScoring = false;
+        int matchScore = configData.getMatchScore();
+        String scoreOptionValue = request.getParameter(MATCH_SCORE);
+
+        if (MATCH_SCORE_CUSTOM.equals(scoreOptionValue)) {
+            useCustomScoring = true;
+            matchScore = getMatchScoreParameter(request);
+        } else if (MATCH_SCORE_TRANSLATION_SERVICE.equals(scoreOptionValue)) {
+            useCustomScoring = false;
+        } else {
+            errors = addError(LABEL_MATCH_SCORE, errors);
+        }
+
+        if (matchScore < 0 || matchScore > 100) {
+            errors = addError(LABEL_MATCH_SCORE, errors);
+        }
+
+        if (errors == null) {
+            configData.setUseCustomScoring(useCustomScoring);
+            if (useCustomScoring) {
+                configData.setMatchScore(matchScore);
+            }
+        }
+        return errors;
+    }
+
+    private int getMatchScoreParameter(HttpServletRequest request) {
+        try {
+            return Integer.valueOf(request.getParameter(MATCH_SCORE_CUSTOM_VALUE));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 }
