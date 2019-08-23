98aa34599a021b96ae62ee2b77e33071ccbcd8e0
Jack Cole <jack@spartansoftwareinc.com>
2019-08-23 13:50:41 -0700

    Restored version 1 of Segmentwhitespace YAML config

--- autoactions/mshub/src/main/java/com/spartansoftwareinc/ws/autoactions/hubmt/config/SegmentWhitespaceFixYAMLConfig.java
+++ autoactions/mshub/src/main/java/com/spartansoftwareinc/ws/autoactions/hubmt/config/SegmentWhitespaceFixYAMLConfig.java
@@ -1,268 +1,74 @@
 package com.spartansoftwareinc.ws.autoactions.hubmt.config;
 
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
-
 public class SegmentWhitespaceFixYAMLConfig {
 
-    public List<SegmentRule> getSegmentRules() {
-        return segmentRules;
-    }
-
-    public void setSegmentRules(List<SegmentRule> segmentRules) {
-        this.segmentRules = segmentRules;
-    }
-
-    private List<SegmentRule> segmentRules;
-
-    private static final String PLACEHOLDER_REGEX = "(\\{(\\d+)\\})";
-
+    private String regex;
+    private Integer leftCaptureGroup;
+    private Integer leftIgnoreCaptureGroup;
+    private Integer centerCaptureGroup;
+    private Integer compareCaptureGroup;
+    private Integer rightIgnoreCaptureGroup;
+    private Integer rightCaptureGroup;
 
     public SegmentWhitespaceFixYAMLConfig() {
     }
 
-    public void construct() {
-        for (SegmentRule segmentRule : segmentRules) {
-            segmentRule.construct();
-        }
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    public Integer getLeftCaptureGroup() {
+        return leftCaptureGroup;
+    }
+
+    public void setLeftCaptureGroup(Integer leftCaptureGroup) {
+        this.leftCaptureGroup = leftCaptureGroup;
+    }
+
+    public Integer getLeftIgnoreCaptureGroup() {
+        return leftIgnoreCaptureGroup;
+    }
+
+    public void setLeftIgnoreCaptureGroup(Integer leftIgnoreCaptureGroup) {
+        this.leftIgnoreCaptureGroup = leftIgnoreCaptureGroup;
+    }
+
+    public Integer getCenterCaptureGroup() {
+        return centerCaptureGroup;
+    }
+
+    public void setCenterCaptureGroup(Integer centerCaptureGroup) {
+        this.centerCaptureGroup = centerCaptureGroup;
     }
 
 
-    public static class SegmentRule {
-
-        // Data from YAML
-        private String sourceLeft;
-        private String targetLeft;
-        private String replacementLeft;
-        private String sourceRight;
-        private String targetRight;
-        private String replacementRight;
-        private Boolean leftRightIndependent;
-        private Boolean allowFurtherReplacementsLeft;
-        private Boolean allowFurtherReplacementsRight;
-        private Set<String> targetLanguages;
-
-        public String getSourceLeft() {
-            return sourceLeft;
-        }
-
-        public void setSourceLeft(String sourceLeft) {
-            this.sourceLeft = sourceLeft;
-        }
-
-        public String getTargetLeft() {
-            return targetLeft;
-        }
-
-        public void setTargetLeft(String targetLeft) {
-            this.targetLeft = targetLeft;
-        }
-
-        public String getReplacementLeft() {
-            return replacementLeft;
-        }
-
-        public void setReplacementLeft(String replacementLeft) {
-            this.replacementLeft = replacementLeft;
-        }
-
-        public String getSourceRight() {
-            return sourceRight;
-        }
-
-        public void setSourceRight(String sourceRight) {
-            this.sourceRight = sourceRight;
-        }
-
-        public String getTargetRight() {
-            return targetRight;
-        }
-
-        public void setTargetRight(String targetRight) {
-            this.targetRight = targetRight;
-        }
-
-        public String getReplacementRight() {
-            return replacementRight;
-        }
-
-        public void setReplacementRight(String replacementRight) {
-            this.replacementRight = replacementRight;
-        }
-
-        public boolean isLeftRightIndependent() {
-            return leftRightIndependent == null || leftRightIndependent;
-        }
-
-        public void setLeftRightIndependent(boolean leftRightIndependent) {
-            this.leftRightIndependent = leftRightIndependent;
-        }
-
-        public boolean isAllowFurtherReplacementsLeft() {
-            return allowFurtherReplacementsLeft == null || allowFurtherReplacementsLeft;
-        }
-
-        public void setAllowFurtherReplacementsLeft(boolean allowFurtherReplacementsLeft) {
-            this.allowFurtherReplacementsLeft = allowFurtherReplacementsLeft;
-        }
-
-        public boolean isAllowFurtherReplacementsRight() {
-            return allowFurtherReplacementsRight == null || allowFurtherReplacementsRight;
-        }
-
-        public void setAllowFurtherReplacementsRight(boolean allowFurtherReplacementsRight) {
-            this.allowFurtherReplacementsRight = allowFurtherReplacementsRight;
-        }
-
-        public Set<String> getTargetLanguages() {
-            return targetLanguages;
-        }
-
-        public void setTargetLanguages(Set<String> targetLanguages) {
-            this.targetLanguages = targetLanguages;
-        }
-
-        public Pattern getTargetPatternRight() {
-            return targetPatternRight;
-        }
-
-        public void setTargetPatternRight(Pattern targetPatternRight) {
-            this.targetPatternRight = targetPatternRight;
-        }
-
-        public Pattern getTargetPatternLeft() {
-            return targetPatternLeft;
-        }
-
-        public void setTargetPatternLeft(Pattern targetPatternLeft) {
-            this.targetPatternLeft = targetPatternLeft;
-        }
-
-        public Pattern getSourcePatternComplete() {
-            return sourcePatternComplete;
-        }
-
-        public void setSourcePatternComplete(Pattern sourcePatternComplete) {
-            this.sourcePatternComplete = sourcePatternComplete;
-        }
-
-        public Pattern getTargetPatternComplete() {
-            return targetPatternComplete;
-        }
-
-        public void setTargetPatternComplete(Pattern targetPatternComplete) {
-            this.targetPatternComplete = targetPatternComplete;
-        }
-
-        public int getSourceCaptureGroupLeft() {
-            return sourceCaptureGroupLeft;
-        }
-
-        public void setSourceCaptureGroupLeft(int sourceCaptureGroupLeft) {
-            this.sourceCaptureGroupLeft = sourceCaptureGroupLeft;
-        }
-
-        public int getSourceCaptureGroupPlaceholder() {
-            return sourceCaptureGroupPlaceholder;
-        }
-
-        public void setSourceCaptureGroupPlaceholder(int sourceCaptureGroupPlaceholder) {
-            this.sourceCaptureGroupPlaceholder = sourceCaptureGroupPlaceholder;
-        }
-
-        public int getSourceCaptureGroupRight() {
-            return sourceCaptureGroupRight;
-        }
-
-        public void setSourceCaptureGroupRight(int sourceCaptureGroupRight) {
-            this.sourceCaptureGroupRight = sourceCaptureGroupRight;
-        }
-
-        public int getTargetCaptureGroupLeft() {
-            return targetCaptureGroupLeft;
-        }
-
-        public void setTargetCaptureGroupLeft(int targetCaptureGroupLeft) {
-            this.targetCaptureGroupLeft = targetCaptureGroupLeft;
-        }
-
-        public int getTargetCaptureGroupPlaceholder() {
-            return targetCaptureGroupPlaceholder;
-        }
-
-        public void setTargetCaptureGroupPlaceholder(int targetCaptureGroupPlaceholder) {
-            this.targetCaptureGroupPlaceholder = targetCaptureGroupPlaceholder;
-        }
-
-        public int getTargetCaptureGroupRight() {
-            return targetCaptureGroupRight;
-        }
-
-        public void setTargetCaptureGroupRight(int targetCaptureGroupRight) {
-            this.targetCaptureGroupRight = targetCaptureGroupRight;
-        }
-
-        // Compiled patterns and capture group numbers
-        private Pattern targetPatternRight;
-        private Pattern targetPatternLeft;
-        private Pattern sourcePatternComplete;
-        private Pattern targetPatternComplete;
-        private int sourceCaptureGroupLeft;
-        private int sourceCaptureGroupPlaceholder;
-        private int sourceCaptureGroupRight;
-        private int targetCaptureGroupLeft;
-        private int targetCaptureGroupPlaceholder;
-        private int targetCaptureGroupRight;
-
-
-        /**
-         * Combines the left and the right REGEX patterns into one giant expression. Even if the provided left and
-         * right patterns don't match, this will still match the pattern.
-         *
-         * @param left  The left REGEX
-         * @param right The right REGEX
-         * @return A fully formed REGEX
-         */
-        private Pattern concatPatterns(String left, String right) {
-            final String fullRegex = String.format("(%s)?%s(%s)?", left, PLACEHOLDER_REGEX, right);
-            return Pattern.compile(fullRegex);
-
-        }
-
-        /**
-         * Gets the capture group index containing the right pattern
-         *
-         * @param fullGroup The entire compiled regular expression
-         * @param right     The right side REGEX pattern
-         * @return The capture group that contains the entire expression
-         */
-        private int getRightCaptureGroupIndex(Pattern fullGroup, String right) {
-            final int totalGroups = fullGroup.matcher("").groupCount();
-            final int rightGroups = right != null ? Pattern.compile(right).matcher("").groupCount() : 0;
-            return totalGroups - rightGroups;
-        }
-
-        public void construct() {
-            if(sourceRight == null)
-                sourceRight = "";
-            if(sourceLeft == null)
-                sourceLeft = "";
-            if(targetRight == null)
-                targetRight = "";
-            if(targetLeft == null)
-                targetLeft = "";
-
-            targetPatternRight = Pattern.compile(targetRight);
-            targetPatternLeft = Pattern.compile(targetLeft);
-            sourcePatternComplete = concatPatterns(sourceLeft, sourceRight);
-            targetPatternComplete = concatPatterns(targetLeft, targetRight);
-            sourceCaptureGroupLeft = 1;
-            targetCaptureGroupLeft = 1;
-            sourceCaptureGroupRight = getRightCaptureGroupIndex(sourcePatternComplete, sourceRight);
-            targetCaptureGroupRight = getRightCaptureGroupIndex(targetPatternComplete, targetRight);
-            sourceCaptureGroupPlaceholder = sourceCaptureGroupRight - 2;
-            targetCaptureGroupPlaceholder = targetCaptureGroupRight - 2;
-        }
+    public Integer getCompareCaptureGroup() {
+        return compareCaptureGroup;
     }
+
+    public void setCompareCaptureGroup(Integer compareCaptureGroup) {
+        this.compareCaptureGroup = compareCaptureGroup;
+    }
+
+    public Integer getRightIgnoreCaptureGroup() {
+        return rightIgnoreCaptureGroup;
+    }
+
+    public void setRightIgnoreCaptureGroup(Integer rightIgnoreCaptureGroup) {
+        this.rightIgnoreCaptureGroup = rightIgnoreCaptureGroup;
+    }
+
+    public Integer getRightCaptureGroup() {
+        return rightCaptureGroup;
+    }
+
+    public void setRightCaptureGroup(Integer rightCaptureGroup) {
+        this.rightCaptureGroup = rightCaptureGroup;
+    }
+
+
 }
---
+++ autoactions/mshub/src/test/java/com/spartansoftwareinc/ws/autoactions/hubmt/SegmentFixMTWhitespaceRemovalTest.java
@@ -0,0 +1,112 @@
+package com.spartansoftwareinc.ws.autoactions.hubmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SegmentFixMTWhitespaceRemovalTest {
+
+    private SegmentFixMTWhitespaceRemoval mtWhitespaceRemoval;
+
+    @Before
+    public void init() throws Exception {
+        mtWhitespaceRemoval = new SegmentFixMTWhitespaceRemoval();
+        mtWhitespaceRemoval.setConfig(null);
+    }
+
+    @Test
+    public void fixWhitespaceTest() throws Exception {
+        testWhitespace("Hello {1}my{2} darling", "Salut{1}disque{2}Darling", "Salut {1}disque{2} Darling");
+        testWhitespace("{0} Hello {1}my{2} darling {999}", "{0}Salut{1}disque{2}Darling{999}", "{0} Salut {1}disque{2} Darling {999}");
+        testWhitespace("{2} what {3} {4} is {5} that {6}", "{2}dangit{3}{4} bobby{5}hill {6}", "{2} dangit {3} {4} bobby {5} hill {6}");
+        testWhitespace("\t\n  {1}   Whoooaaa theree buddy\t\t\n{2}\t{3}\n\nThat ain't{4}\t\tcool!{5}", "{1}Test test test{2}{3}ahahhaha{4}awesome!{5}", "\t\n  {1}   Test test test\t\t\n{2}\t{3}\n\nahahhaha{4}\t\tawesome!{5}");
+
+        testWhitespace("\t \n  {1}     Whoooaaa theree buddy\t\t\n{2}\t  {3}  \n\nThat ain't{4}\t\tcool!{5} ", "{1}Test test test{2}{3}ahahhaha{4}awesome!{5}", "\t \n  {1}     Test test test\t\t\n{2}\t  {3}  \n\nahahhaha{4}\t\tawesome!{5} ");
+
+        // Every single space found on https://unicode-table.com/en/search/?q=space. There might be some duplicates mixed in.
+        testWhitespace("Hello"+
+                " \t\n\u2006\u00A0\u2002\u2003\u2003\u202F  \u200B \uDB40\uDC20         　    " +
+                "{150002023023}" +
+                " \t\n\u2006\u00A0\u2002\u2003\u2003\u202F  \u200B \uDB40\uDC20         　    "+
+                "there",
+            "Bonjour{150002023023}Là",
+            "Bonjour"+
+                " \t\n\u2006\u00A0\u2002\u2003\u2003\u202F  \u200B \uDB40\uDC20         　    " +
+                "{150002023023}" +
+                " \t\n\u2006\u00A0\u2002\u2003\u2003\u202F  \u200B \uDB40\uDC20         　    "+
+                "Là");
+
+        testWhitespace("From the {1}Transfer Funds{2} {3}From{4} drop-down, choose the bank account the credit card was paid from.",
+
+            "Dans la liste déroulante {1}Transférer des fonds{2} {3}depuis{4}, choisissez le compte bancaire d'où le paiement a été émis.",
+            "Dans la liste déroulante {1}Transférer des fonds{2} {3}depuis{4}, choisissez le compte bancaire d'où le paiement a été émis.");
+
+        testWhitespace("{1}This is one long sentence{2} in order to {3}test{4} punctuation {5}detection{6} which is {7}quite{8} rare.",
+
+            "{1}This is now punctuated{2}, in order to {3}test!{4} Punctuation {5}detection,{6} while somewhat strange, is {7}important{8}!",
+            "{1}This is now punctuated{2}, in order to {3}test!{4} Punctuation {5}detection,{6} while somewhat strange, is {7}important{8}!");
+
+        testWhitespace("{1}This is one long sentence{2} in order to {3}test{4} punctuation {5}detection{6} which is {7}quite{8} rare.",
+
+            "{1}This is now punctuated{2}¿ In order to {3}test¡{4} Punctuation {5}detection¿{6} while somewhat strange, is {7}important{8}¡",
+            "{1}This is now punctuated{2}¿ In order to {3}test¡{4} Punctuation {5}detection¿{6} while somewhat strange, is {7}important{8}¡");
+
+        testWhitespace("{1}reorder{2} {3}test{4}.", "{3}test{4} {1}reorder{2}.", "{3}test{4}{1}reorder{2}.");
+
+        // Theoretical sentence beginning/end case
+        //        testWhitespace("{1}Beginning and end{2}.",
+        //
+        //                "Not {1}beginning{2} or end.",
+        //                "Not {1}beginning{2} or end.");
+        //        testWhitespace("Not {1}beginning{2} or end.",
+        //                "{1}Beginning and end{2}.",
+        //                "{1}Beginning and end{2}.");
+
+
+    }
+
+    @Test
+    public void testConfigLoading() throws Exception {
+
+        InputStream config = mtWhitespaceRemoval.loadConfigFromResources();
+        java.util.Scanner s = new java.util.Scanner(config).useDelimiter("\\A");
+        StringBuilder configString = new StringBuilder();
+        while (s.hasNext()) {
+            configString.append(s.next());
+        }
+        config.close();
+        // Check regex valid
+        String regex = mtWhitespaceRemoval.config.getRegex();
+        assertNotNull(regex);
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(" {123} ");
+        assertEquals(6, matcher.groupCount());
+
+        // Check other params
+        assertEquals(mtWhitespaceRemoval.config.getLeftCaptureGroup(), new Integer(1));
+        assertEquals(mtWhitespaceRemoval.config.getLeftIgnoreCaptureGroup(), new Integer(2));
+        assertEquals(mtWhitespaceRemoval.config.getCenterCaptureGroup(), new Integer(3));
+        assertEquals(mtWhitespaceRemoval.config.getCompareCaptureGroup(), new Integer(4));
+        assertEquals(mtWhitespaceRemoval.config.getRightIgnoreCaptureGroup(), new Integer(5));
+        assertEquals(mtWhitespaceRemoval.config.getRightCaptureGroup(), new Integer(6));
+    }
+
+    /**
+     * @param source Original string
+     * @param target Translated string
+     * @param fixed  The proper translated string
+     */
+    private void testWhitespace(final String source, final String target, final String fixed) {
+
+        String test_fixed = mtWhitespaceRemoval.fixWhitespace(source, target);
+        assertEquals(fixed, test_fixed);
+    }
+
+
+}
