ecb47387edf69c1cd0462d1eb96ccc72320d969a
Jack Cole <jack@spartansoftwareinc.com>
2020-03-23 12:14:37 -0700

    Enhanced Simple Post Edit Auto Action to be able to disallow further replacements.

--- autoactions/mshub/src/test/java/com/spartansoftwareinc/ws/autoactions/hubmt/SimplePostEditAutoActionTest.java
+++ autoactions/mshub/src/test/java/com/spartansoftwareinc/ws/autoactions/hubmt/SimplePostEditAutoActionTest.java
@@ -34,6 +34,38 @@ public class SimplePostEditAutoActionTest {
         testUpdate(actions, "Cet article a-t-il été utile\u00A0?", "Cet article a-t-il été utile\u00A0?");
     }
 
+
+    @Test
+    public void testURLs() throws Exception {
+        final List<SimplePostEditAutoActionYAMLConfig.Action> frenchFranceActions = config.getActionsForLanguage("French (France)");
+        testUpdate(frenchFranceActions, "https://www.youtube.com/watch?v=abc212_AVC",
+            "https://www.youtube.com/watch?v=abc212_AVC");
+//        testUpdate(frenchFranceActions, "The URL is {1}https\u00A0://www.youtube.com/watch\u00A0?v=abc212_AVC{2}.",
+//            "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}.");
+        testUpdate(frenchFranceActions, "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}.",
+            "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}.");
+
+        final List<SimplePostEditAutoActionYAMLConfig.Action> frenchCanadaActions = config.getActionsForLanguage(
+            "French (Canada)");
+        testUpdate(frenchCanadaActions, "https://www.youtube.com/watch?v=abc212_AVC",
+            "https://www.youtube.com/watch?v=abc212_AVC");
+//        testUpdate(frenchFranceActions, "The URL is {1}https\u00A0://www.youtube.com/watch?v=abc212_AVC{2}.",
+//            "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}.");
+        testUpdate(frenchCanadaActions, "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}. This: is a "
+                + "sentence?",
+            "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}. This\u00A0: is a "
+            + "sentence?");
+
+
+        final List<SimplePostEditAutoActionYAMLConfig.Action> germanGermanyActions = config.getActionsForLanguage(
+            "German (Germany)");
+        testUpdate(germanGermanyActions, "https://www.youtube.com/watch?v=abc212_AVC",
+            "https://www.youtube.com/watch?v=abc212_AVC");
+        testUpdate(germanGermanyActions, "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}. This: is a "
+                + "sentence?",
+            "The URL is {1}https://www.youtube.com/watch?v=abc212_AVC{2}. This: is a sentence?");
+    }
+
     /**
      * @param actions The actions to perform
      * @param input   Original string
